e4ec373bcb1ca476c8fbae567290993ed8e893fa
Ertuğrul Özer <212002002@ogrenci.amasya.edu.tr>
2022-12-17 18:03:07 +0300

    ewrg

--- WindowsFormsApp2/yeniKayit.Designer.cs
+++ WindowsFormsApp2/yeniKayit.Designer.cs
@@ -34,12 +34,13 @@ namespace WindowsFormsApp2
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(366, 345);
+            this.button1.Location = new System.Drawing.Point(350, 39);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // yeniKayit
             // 
