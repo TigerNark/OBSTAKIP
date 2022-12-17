539047dd9cce76ab4f03c175b43619921ac761ab
Ertuğrul Özer <212002002@ogrenci.amasya.edu.tr>
2022-12-17 18:10:18 +0300

    yeni kayıt tasarım güncellemesi

--- WindowsFormsApp2/yeniKayit.Designer.cs
+++ WindowsFormsApp2/yeniKayit.Designer.cs
@@ -34,7 +34,7 @@ namespace WindowsFormsApp2
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(350, 39);
+            this.button1.Location = new System.Drawing.Point(656, 251);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
