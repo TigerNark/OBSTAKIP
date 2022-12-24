49b715e948edf7a42407df4f6ff4927fc465eff5
ProjeTea <120734111+ProjeTea@users.noreply.github.com>
2022-12-24 20:04:00 +0300

    24.12 20:03 DERSEKLE TAMAMLANDİ

--- WindowsFormsApp2/dersekle.Designer.cs
+++ WindowsFormsApp2/dersekle.Designer.cs
@@ -33,6 +33,27 @@ namespace WindowsFormsApp2
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.sekiz = new System.Windows.Forms.TextBox();
+            this.dokuz = new System.Windows.Forms.TextBox();
+            this.on = new System.Windows.Forms.TextBox();
+            this.iki = new System.Windows.Forms.TextBox();
+            this.bir = new System.Windows.Forms.TextBox();
+            this.onbir = new System.Windows.Forms.TextBox();
+            this.dört = new System.Windows.Forms.TextBox();
+            this.üç = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.gun = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.oniki = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -51,7 +72,7 @@ namespace WindowsFormsApp2
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.button1.Location = new System.Drawing.Point(837, 39);
+            this.button1.Location = new System.Drawing.Point(819, 129);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(98, 38);
             this.button1.TabIndex = 46;
@@ -62,16 +83,209 @@ namespace WindowsFormsApp2
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(55, 111);
+            this.dataGridView1.Location = new System.Drawing.Point(39, 207);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(870, 323);
+            this.dataGridView1.Size = new System.Drawing.Size(896, 231);
             this.dataGridView1.TabIndex = 47;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // sekiz
+            // 
+            this.sekiz.Location = new System.Drawing.Point(306, 50);
+            this.sekiz.Name = "sekiz";
+            this.sekiz.Size = new System.Drawing.Size(100, 20);
+            this.sekiz.TabIndex = 48;
+            // 
+            // dokuz
+            // 
+            this.dokuz.Location = new System.Drawing.Point(306, 97);
+            this.dokuz.Name = "dokuz";
+            this.dokuz.Size = new System.Drawing.Size(100, 20);
+            this.dokuz.TabIndex = 49;
+            // 
+            // on
+            // 
+            this.on.Location = new System.Drawing.Point(306, 142);
+            this.on.Name = "on";
+            this.on.Size = new System.Drawing.Size(100, 20);
+            this.on.TabIndex = 50;
+            // 
+            // iki
+            // 
+            this.iki.Location = new System.Drawing.Point(691, 50);
+            this.iki.Name = "iki";
+            this.iki.Size = new System.Drawing.Size(100, 20);
+            this.iki.TabIndex = 51;
+            // 
+            // bir
+            // 
+            this.bir.Location = new System.Drawing.Point(489, 140);
+            this.bir.Name = "bir";
+            this.bir.Size = new System.Drawing.Size(100, 20);
+            this.bir.TabIndex = 52;
+            // 
+            // onbir
+            // 
+            this.onbir.Location = new System.Drawing.Point(489, 50);
+            this.onbir.Name = "onbir";
+            this.onbir.Size = new System.Drawing.Size(100, 20);
+            this.onbir.TabIndex = 53;
+            // 
+            // dört
+            // 
+            this.dört.Location = new System.Drawing.Point(691, 142);
+            this.dört.Name = "dört";
+            this.dört.Size = new System.Drawing.Size(100, 20);
+            this.dört.TabIndex = 54;
+            // 
+            // üç
+            // 
+            this.üç.Location = new System.Drawing.Point(691, 96);
+            this.üç.Name = "üç";
+            this.üç.Size = new System.Drawing.Size(100, 20);
+            this.üç.TabIndex = 55;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(230, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 56;
+            this.label1.Text = "08:00 - 08:45";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(230, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 57;
+            this.label2.Text = "09:00 - 09:45";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(230, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 58;
+            this.label3.Text = "10:00 - 10:45";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(413, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 59;
+            this.label4.Text = "11:00 - 11:45";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(413, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 60;
+            this.label5.Text = "13:00 - 13:45";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(615, 52);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 13);
+            this.label6.TabIndex = 61;
+            this.label6.Text = "14:00 - 14:45";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(615, 99);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 13);
+            this.label7.TabIndex = 62;
+            this.label7.Text = "15:00 -15:45";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(615, 145);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(70, 13);
+            this.label8.TabIndex = 63;
+            this.label8.Text = "16:00 - 16:45";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.button3.Location = new System.Drawing.Point(819, 52);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 38);
+            this.button3.TabIndex = 82;
+            this.button3.Text = "GÜNCELLE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // gun
+            // 
+            this.gun.Location = new System.Drawing.Point(76, 100);
+            this.gun.Name = "gun";
+            this.gun.Size = new System.Drawing.Size(100, 20);
+            this.gun.TabIndex = 83;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(36, 104);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(34, 13);
+            this.label9.TabIndex = 84;
+            this.label9.Text = "GÜN:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(413, 100);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(70, 13);
+            this.label10.TabIndex = 86;
+            this.label10.Text = "12:00 - 12:45";
+            // 
+            // oniki
+            // 
+            this.oniki.Location = new System.Drawing.Point(489, 97);
+            this.oniki.Name = "oniki";
+            this.oniki.Size = new System.Drawing.Size(100, 20);
+            this.oniki.TabIndex = 85;
             // 
             // dersEkle
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(965, 462);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.oniki);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.gun);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.üç);
+            this.Controls.Add(this.dört);
+            this.Controls.Add(this.onbir);
+            this.Controls.Add(this.bir);
+            this.Controls.Add(this.iki);
+            this.Controls.Add(this.on);
+            this.Controls.Add(this.dokuz);
+            this.Controls.Add(this.sekiz);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
@@ -83,6 +297,7 @@ namespace WindowsFormsApp2
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -90,5 +305,26 @@ namespace WindowsFormsApp2
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox sekiz;
+        private System.Windows.Forms.TextBox dokuz;
+        private System.Windows.Forms.TextBox on;
+        private System.Windows.Forms.TextBox iki;
+        private System.Windows.Forms.TextBox bir;
+        private System.Windows.Forms.TextBox onbir;
+        private System.Windows.Forms.TextBox dört;
+        private System.Windows.Forms.TextBox üç;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox gun;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox oniki;
     }
 }
--- WindowsFormsApp2/dersekle.cs
+++ WindowsFormsApp2/dersekle.cs
@@ -53,5 +53,40 @@ namespace WindowsFormsApp2
             a.Show();
             this.Close();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            OleDbCommand komut = new OleDbCommand("UPDATE [dersProgrami$] set BİRİNCİDERS=@p2,İKİNCİDERS=@p3,ÜÇÜNCÜDERS=@p4,DÖRDÜNCÜDERS=@p5,ÖGLEARASI=@p0,BEŞİNCİDERS=@p6,ALTINCIDERS=@p7,YEDİNCİDERS=@p8,SEKİZİNCİDERS=@p9 WHERE GÜNLER=@p1", baglanti);
+            komut.Parameters.AddWithValue("@p2", sekiz.Text);
+            komut.Parameters.AddWithValue("@p3", dokuz.Text);
+            komut.Parameters.AddWithValue("@p4", on.Text);
+            komut.Parameters.AddWithValue("@p5", onbir.Text);
+            komut.Parameters.AddWithValue("@p0", oniki.Text);
+            komut.Parameters.AddWithValue("@p6", bir.Text);
+            komut.Parameters.AddWithValue("@p7", iki.Text);
+            komut.Parameters.AddWithValue("@p8", üç.Text);
+            komut.Parameters.AddWithValue("@p9", dört.Text);
+            komut.Parameters.AddWithValue("@p1", gun.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Ders Güncellendi");
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int secilen = dataGridView1.SelectedCells[0].RowIndex;
+
+            gun.Text = dataGridView1.Rows[secilen].Cells[0].Value.ToString();
+            sekiz.Text = dataGridView1.Rows[secilen].Cells[1].Value.ToString();
+            dokuz.Text = dataGridView1.Rows[secilen].Cells[2].Value.ToString();
+            on.Text = dataGridView1.Rows[secilen].Cells[3].Value.ToString();
+            onbir.Text = dataGridView1.Rows[secilen].Cells[4].Value.ToString();
+            oniki.Text = dataGridView1.Rows[secilen].Cells[5].Value.ToString();
+            bir.Text = dataGridView1.Rows[secilen].Cells[6].Value.ToString();
+            iki.Text = dataGridView1.Rows[secilen].Cells[7].Value.ToString();
+            üç.Text = dataGridView1.Rows[secilen].Cells[8].Value.ToString();
+            dört.Text = dataGridView1.Rows[secilen].Cells[9].Value.ToString();
+        }
     }
 }
