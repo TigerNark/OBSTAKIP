1b06af693ad1c21dfb367158102d966fff36a079
ProjeTea <120734111+ProjeTea@users.noreply.github.com>
2022-12-26 07:42:49 +0300

    ö

--- WindowsFormsApp2/notGiris.cs
+++ WindowsFormsApp2/notGiris.cs
@@ -199,95 +199,95 @@ namespace WindowsFormsApp2
                 fizso.Text = "KALDI";
             }
             fizort.Text = Sonuc.ToString();
-        } 
-           /* void button2_Click()
-            {
+        }
+        /* void button2_Click()
+         {
 
-                // kaydet butonu
+             // kaydet butonu
 
-                /*
-                string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=obstakip.accdb;";
+             /*
+             string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=obstakip.accdb;";
 
-                using (OleDbConnection connection = new OleDbConnection(connectionString))
-                {
-                    connection.Open();
+             using (OleDbConnection connection = new OleDbConnection(connectionString))
+             {
+                 connection.Open();
 
-                    string query = "INSERT INTO notlar (ogrenciNo, biyo1, biyo2) VALUES (@ogrenciNo, @biyo1, @biyo2)";
+                 string query = "INSERT INTO notlar (ogrenciNo, biyo1, biyo2) VALUES (@ogrenciNo, @biyo1, @biyo2)";
 
-                    using (OleDbCommand command1 = new OleDbCommand(query, connection))
-                    {
+                 using (OleDbCommand command1 = new OleDbCommand(query, connection))
+                 {
 
-                        command1.Parameters.AddWithValue("@biyo1", v1.Text);
-                        command1.Parameters.AddWithValue("@biyo2", f1.Text);
-                        command1.ExecuteNonQuery();
-                    }
-                }*/
-
-
+                     command1.Parameters.AddWithValue("@biyo1", v1.Text);
+                     command1.Parameters.AddWithValue("@biyo2", f1.Text);
+                     command1.ExecuteNonQuery();
+                 }
+             }*/
 
 
 
 
-                /*
-                OleDbConnection baglanti = new OleDbConnection("Provider=Microsoft.ACE.Oledb.12.0;Data Source=obstakip.accdb");
 
 
-                baglanti.Open();// bağlantı açıldı
-                //sorgu komut yapısı oluşturuldu
-                 OleDbCommand sorgu = new OleDbCommand()
-                {
-
-                }
-
-                sorgu.Connection = baglanti;
+        /*
+        OleDbConnection baglanti = new OleDbConnection("Provider=Microsoft.ACE.Oledb.12.0;Data Source=obstakip.accdb");
 
 
-                string vize = v1.Text;
-                string final = f1.Text;
+        baglanti.Open();// bağlantı açıldı
+        //sorgu komut yapısı oluşturuldu
+         OleDbCommand sorgu = new OleDbCommand()
+        {
+
+        }
+
+        sorgu.Connection = baglanti;
 
 
-                sorgu.CommandType = CommandType.Text;
-                //string query = "INSERT INTO notlar (ogrenciNo, biyo1,biyo2,fizik1,fizik2,kimya1,kimya2,lab1,lab2,prog1,prog2) values (@ogrenciNo, @biyo1,@biyo2,@fizik1,@fizik2,@kimya1,@kimya2,@lab1,@lab2,@prog1,@prog2)";
+        string vize = v1.Text;
+        string final = f1.Text;
 
 
-                if (biyo.Enabled == true && v1.Enabled == true && f1.Enabled == true)
-                {
-                    sorgu.CommandText = "insert into notlar (ogrenciNo, biyo1,biyo2) values (@ogrenciNo,@biyo1,@biyo2)";
-                    sorgu.Parameters.AddWithValue("@ogrenciNo", sorgulananOgrenciNo.Text);
-                    sorgu.Parameters.AddWithValue("@biyo1", vize);
-                    sorgu.Parameters.AddWithValue("@biyo2", final);
-                    //sorgu.ExecuteNonQuery();
-                }
-
-                else if(fizik.Enabled == true && v1.Enabled==true && f1.Enabled==true)
-                {
-                    sorgu.Parameters.AddWithValue("@fizik1", vize);
-                    sorgu.Parameters.AddWithValue("@fizik2", final);
-                }
-                else if(kimya.Enabled == true)
-                {
-                    sorgu.Parameters.AddWithValue("@kimya1", vize);
-                    sorgu.Parameters.AddWithValue("@kimya2", final);
-                }
-                else if(lab.Enabled== true)
-                {
-                    sorgu.Parameters.AddWithValue("@lab1", vize);
-                    sorgu.Parameters.AddWithValue("@lab2", final);
-                }
-                else if(prog.Enabled == true)
-                {
-                    sorgu.Parameters.AddWithValue("@prog1", vize);
-                    sorgu.Parameters.AddWithValue("@prog2", final);
-                }
+        sorgu.CommandType = CommandType.Text;
+        //string query = "INSERT INTO notlar (ogrenciNo, biyo1,biyo2,fizik1,fizik2,kimya1,kimya2,lab1,lab2,prog1,prog2) values (@ogrenciNo, @biyo1,@biyo2,@fizik1,@fizik2,@kimya1,@kimya2,@lab1,@lab2,@prog1,@prog2)";
 
 
+        if (biyo.Enabled == true && v1.Enabled == true && f1.Enabled == true)
+        {
+            sorgu.CommandText = "insert into notlar (ogrenciNo, biyo1,biyo2) values (@ogrenciNo,@biyo1,@biyo2)";
+            sorgu.Parameters.AddWithValue("@ogrenciNo", sorgulananOgrenciNo.Text);
+            sorgu.Parameters.AddWithValue("@biyo1", vize);
+            sorgu.Parameters.AddWithValue("@biyo2", final);
+            //sorgu.ExecuteNonQuery();
+        }
 
-                MessageBox.Show("Not Değişiklikleri Yapılmıştır.");
-                baglanti.Close();// bağlantı kapatıldı.
+        else if(fizik.Enabled == true && v1.Enabled==true && f1.Enabled==true)
+        {
+            sorgu.Parameters.AddWithValue("@fizik1", vize);
+            sorgu.Parameters.AddWithValue("@fizik2", final);
+        }
+        else if(kimya.Enabled == true)
+        {
+            sorgu.Parameters.AddWithValue("@kimya1", vize);
+            sorgu.Parameters.AddWithValue("@kimya2", final);
+        }
+        else if(lab.Enabled== true)
+        {
+            sorgu.Parameters.AddWithValue("@lab1", vize);
+            sorgu.Parameters.AddWithValue("@lab2", final);
+        }
+        else if(prog.Enabled == true)
+        {
+            sorgu.Parameters.AddWithValue("@prog1", vize);
+            sorgu.Parameters.AddWithValue("@prog2", final);
+        }
 
-                
-            }*/
-        
+
+
+        MessageBox.Show("Not Değişiklikleri Yapılmıştır.");
+        baglanti.Close();// bağlantı kapatıldı.
+
+
+    }*/
+
 
         private void lab_Click(object sender, EventArgs e)
         {
@@ -356,16 +356,16 @@ namespace WindowsFormsApp2
         {
 
             // kaydet butonu
-            
+
 
 
             OleDbConnection baglanti = new OleDbConnection("Provider=Microsoft.ACE.Oledb.12.0;Data Source=obstakip.accdb");
 
             baglanti.Open();
             OleDbCommand komut = new OleDbCommand();
-            komut.Connection=baglanti;
-            komut.CommandText = "update notlar set mat1='" + mv.Text + "',mat2='" + mf.Text + "',fizik1='" + fv.Text + "',fizik2='" + ff.Text + "',kimya1='" + kv.Text + "',kimya2='" + kf.Text + "',lab1='" + lv.Text + "',lab2='" + lf.Text + "',prog1='" + pv.Text + "',prog2='" + pf.Text + "' where ogrenciNo='" + sorgulananOgrenciNo.Text + "'";                 
-           
+            komut.Connection = baglanti;
+            komut.CommandText = "update notlar set mat1='" + mv.Text + "',mat2='" + mf.Text + "',fizik1='" + fv.Text + "',fizik2='" + ff.Text + "',kimya1='" + kv.Text + "',kimya2='" + kf.Text + "',lab1='" + lv.Text + "',lab2='" + lf.Text + "',prog1='" + pv.Text + "',prog2='" + pf.Text + "' where ogrenciNo='" + sorgulananOgrenciNo.Text + "'";
+
 
             /*komut.Parameters.AddWithValue("@p1", mv.Text);
             komut.Parameters.AddWithValue("@p2", mf.Text);
